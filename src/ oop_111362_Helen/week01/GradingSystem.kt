b9adb17762927937875d7c26583c81dd2eb35f35
Helen <helenhendra08@gmail.com>
2026-02-12 23:34:40 +0700

    week01: refactor to use val and string templates

--- src/ oop_111362_Helen/week01/GradingSystem.kt
+++ src/ oop_111362_Helen/week01/GradingSystem.kt
@@ -1,2 +1,9 @@
 package ` oop_111362_Helen`.week01
 
+fun main() {
+    val name: String = "John Thor"
+    val score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
