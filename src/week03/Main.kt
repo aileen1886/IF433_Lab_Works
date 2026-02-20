303f25ed2b22822227105cf61c8fc65bc4bd75bc
Helen <helenhendra08@gmail.com>
2026-02-20 11:28:00 +0700

    week03: (task 1) implement Weapon testing in main

--- src/week03/Main.kt
+++ src/week03/Main.kt
@@ -2,12 +2,16 @@ package week03
 
 fun main() {
     val e = Employee("Budi")
-
     e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
-
     e.increasePerformance()
-
     println("Pajak yang harus dibayar: ${e.tax}")
+
+    println("\n--- Test Weapon Task (Checkpoint 8) ---")
+    val w = Weapon("Keris")
+    w.damage = -50
+    w.damage = 9999
+
+    println("Weapon: ${w.name}, Damage: ${w.damage}, Tier: ${w.tier}")
 }
