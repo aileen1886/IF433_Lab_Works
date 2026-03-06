5304aae363551ba97ffeec16c30e0f3f82c9ad35
Helen <helenhendra08@gmail.com>
2026-03-07 00:02:42 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/week5/Main.kt
+++ src/week5/Main.kt
@@ -1,6 +1,19 @@
 package week5
 
 fun main() {
+    val math = MathHelper()
+
+    println("=== PENGUJIAN OVERLOADING ===")
+
+    val luasPersegi = math.hitungLuas(5)
+    println("Luas Persegi (sisi 5): $luasPersegi")
+
+    val luasPersegiPanjang = math.hitungLuas(10, 5)
+    println("Luas Persegi Panjang (10x5): $luasPersegiPanjang")
+
+    val luasLingkaran = math.hitungLuas(7.0)
+    println("Luas Lingkaran (r 7): $luasLingkaran")
+
     val dosen1 = Dosen(nama = "Pak Alex", nidn = "0123456")
     val admin1 = Admin(nama = "Bu Siti")
 
