bcebb403ed7839e8f633cac837f818cc667b2efd
Helen <helenhendra08@gmail.com>
2026-03-13 10:48:45 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/111362_Helen_week6/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package `111362_Helen_week6`
+
+interface Camera {
+    fun turnon() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnon() { println("Sinyal seluler mencari jaringan.") }
+}
