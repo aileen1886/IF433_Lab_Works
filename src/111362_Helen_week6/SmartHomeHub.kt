568f9bce20ea2e5ab5453de4a98b5ffd41ac94f5
Helen <helenhendra08@gmail.com>
2026-03-13 11:22:54 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/111362_Helen_week6/SmartHomeHub.kt
+++ src/111362_Helen_week6/SmartHomeHub.kt
@@ -6,7 +6,16 @@ class SmartHomeHub {
     fun addDevice(device: SmartDevice) {
         devices.add(device)
     }
-
+    fun activateSecurityMode() { // [cite: 169]
+        for (device in devices) { // [cite: 169]
+            if (device is Recordable) { // [cite: 170]
+                device.startRecord() // [cite: 170]
+            }
+            if (device is SmartSpeaker) { // [cite: 170, 171]
+                device.playMusic("Sirine Peringatan") // [cite: 171]
+            }
+        }
+    }
     fun turnOffAllSwitches() {
         for (device in devices) {
             if (device is Switchable) {
