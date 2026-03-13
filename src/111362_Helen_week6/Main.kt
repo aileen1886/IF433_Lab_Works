4151015fb9816dcc36c2d4412517c47d77ff4d6f
Helen <helenhendra08@gmail.com>
2026-03-13 11:25:19 +0700

    week06: (task) instantiate smart devices in main

--- src/111362_Helen_week6/Main.kt
+++ src/111362_Helen_week6/Main.kt
@@ -18,4 +18,9 @@ fun main() {
     println("\n=== TESTING CHECKOUT ===")
     processCheckout(method = pay1, amount = 50000.0)
     processCheckout(method = pay2, amount = 150000.0)
+
+    println("\n=== TESTING SMART HOME ===")
+    val lamp = SmartLamp("L01", "Ruang Tamu")
+    val speaker = SmartSpeaker("S01", "Google Nest Dapur")
+    val cctv = SmartCCTV("C01", "Ezviz Garasi")
 }
