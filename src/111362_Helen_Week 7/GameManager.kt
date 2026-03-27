884dd3f59d9f2fe306165c7d8ced3f27e9505f0b
Helen <helenhendra08@gmail.com>
2026-03-27 11:25:30 +0700

    week07: (task) create GameManager singleton

---
+++ src/111362_Helen_Week 7/GameManager.kt
@@ -0,0 +1,13 @@
+package `111362_Helen_Week 7`
+
+object GameManager {
+    var isGameRunning: Boolean = false
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
