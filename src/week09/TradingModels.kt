33018aacb3c215d142e2a0d9b3ced08806394fa7
Helen <helenhendra08@gmail.com>
2026-04-24 10:47:52 +0700

    week09: create basic lambdas using explicit and implicit syntax

--- src/week09/TradingModels.kt
+++ src/week09/TradingModels.kt
@@ -1,10 +1,9 @@
 package week09
 
-// Data class untuk menyimpan log perdagangan
 data class TradeLog(
     val pair: String,
     val position: String,
     val leverage: Int,
     val roe: Double,
     val status: String
-) [cite: 138, 139]
+)
