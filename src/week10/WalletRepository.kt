30eefa5d143f6549d12b3525f9dd89d591e3d255
Helen <helenhendra08@gmail.com>
2026-05-08 10:59:04 +0700

    week10: (task) implement search logic in repository

--- src/week10/WalletRepository.kt
+++ src/week10/WalletRepository.kt
@@ -2,7 +2,13 @@ package week10
 
 class WalletRepository<T> {
     private val items = mutableListOf<T>()
-
-    fun add(item: T) { items.add(item) }
+    fun add(item: T) {
+        items.add(item)
+    }
     fun getAll(): List<T> = items
+    fun searchByName(query: String): List<T> {
+        return items.filter { item ->
+            item.toString().contains(query, ignoreCase = true)
+        }
+    }
 }
