257ec504e69ec124511bd3a1a6121ed43edf3a86
Helen <helenhendra08@gmail.com>
2026-05-14 22:05:47 +0700

    week11: test extension functions in main

---
+++ src/week11/Main.kt
@@ -0,0 +1,10 @@
+package week11
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
