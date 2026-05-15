272e6da837ed510e4d8a156355e18e07a66d54aa
Helen <helenhendra08@gmail.com>
2026-05-15 11:13:39 +0700

    week12: (task) test full robust smart feeder pipeline

--- src/week12/SmartFeeder.kt
+++ src/week12/SmartFeeder.kt
@@ -30,4 +30,14 @@ fun main() {
     } finally {
         println("Siklus pengecekan dispenser pagi selesai.")
     }
+    println("\n--- JADWAL MAKAN SORE ---")
+    runCatching {
+        dispenseKibble(30, 1000, false)
+    }.onSuccess { newStock ->
+        currentKibbleStock = newStock
+        println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
+    }.onFailure { error ->
+        println("Peringatan ke Pemilik: ${error.message}")
+        println("(Opsional: Berikan chicken jerky secara manual)")
+    }
 }
