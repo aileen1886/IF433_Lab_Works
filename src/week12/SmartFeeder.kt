3895e52b9d85270caec10bdd071e5504b8599dc7
Helen <helenhendra08@gmail.com>
2026-05-15 11:03:35 +0700

    week12: (task) implement dispenseKibble function

--- src/week12/SmartFeeder.kt
+++ src/week12/SmartFeeder.kt
@@ -1,8 +1,6 @@
 package week12
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
-    // Validasi porsi harus > 0
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
-    // Placeholder return (akan diupdate di step berikutnya)
     return availableGram
 }
